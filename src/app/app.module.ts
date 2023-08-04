2f018faec51c3170e0e1e81bb25646f49b3df7b4
Sebastian Capurro <sebacapu3@gmail.com>
2023-08-03 21:04:50 -0300

    creo componentes base para la app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CurrencyListComponent } from './components/currency-list/currency-list.component';
+import { NavComponent } from './components/nav/nav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MarketOverviewComponent } from './components/market-overview/market-overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrencyListComponent,
+    NavComponent,
+    MarketOverviewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/market-overview/market-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-market-overview',
+  templateUrl: './market-overview.component.html',
+  styleUrls: ['./market-overview.component.scss']
+})
+export class MarketOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
